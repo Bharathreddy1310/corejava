e38a7ffdc3a4f01a10d8661cc2064d273af13f53
CSELAB6 <CSELAB6@DESKTOP-PRQQPSQ>
2025-01-31 06:15:02 +0530

    inheri

---
+++ corejavaprogram/src/com/tnsif/inheritance/Child.java
@@ -0,0 +1,17 @@
+package com.tnsif.inheritance;
+
+public class Child extends Father{
+	
+	String color="white";
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Child c=new Child();
+		System.out.println(c.cost);
+		System.out.println(c.car);
+		System.out.println(c.color);
+		
+		
+	}
+
+}
---
+++ corejavaprogram/src/com/tnsif/inheritance/Father.java
@@ -0,0 +1,8 @@
+package com.tnsif.inheritance;
+
+public class Father {
+
+	String car="ford";
+	int cost=500;
+			
+}
